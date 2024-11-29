80297b890f14cb58d6dbd59d5073fd5e5fd0c332
Perez Benjamin <benjaminperez1@outlook.fr>
2024-11-29 23:11:06 +0100

    question 13

--- collec_management/admin.py
+++ collec_management/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Collec
+from .models import Collec, Element
 
 # Register your models here.
 admin.site.register(Collec)
+admin.site.register(Element)
+
