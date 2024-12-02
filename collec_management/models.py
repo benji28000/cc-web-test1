e13416377cb665b9f992bfc5d59eaacdcf935f2a
Perez Benjamin <benjamin.perez@etu.univ-orleans.fr>
2024-12-02 18:34:12 +0100

    detail

--- collec_management/models.py
+++ collec_management/models.py
@@ -7,7 +7,7 @@ class Collec(models.Model):
     title = models.CharField(max_length=100,null=False)
     description = models.TextField(null=False)
     date = models.DateTimeField(auto_now_add=True,null=False)
-    user = models.ForeignKey(User,on_delete=models.CASCADE, related_name='collections', null=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE, related_name='collections')
 
     def __str__(self):
         return self.title
@@ -19,7 +19,7 @@ class Element(models.Model):
     quantity = models.IntegerField(null=False)
     date = models.DateTimeField(auto_now_add=True,null=False)
     collec = models.ForeignKey(Collec,on_delete=models.CASCADE, related_name='elements')
-    user = models.ForeignKey(User,on_delete=models.CASCADE, related_name='elements', null=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE, related_name='elements')
 
     def __str__(self):
         return self.title
